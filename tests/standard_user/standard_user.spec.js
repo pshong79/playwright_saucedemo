8ada57fa854e2b2e544165839fb890fb8ec4fcaa
-- <-->
2024-02-25 20:23:36 -0600

    Standard user: checkout

--- tests/standard_user/standard_user.spec.js
+++ tests/standard_user/standard_user.spec.js
@@ -33,19 +33,63 @@ test.describe('standard_user', () => {
   test('add product to cart by product details', async ({ page }) => {
     // Product = Test.allTheThings() T-Shirt (Red)
     let product = 'Test.allTheThings() T-Shirt (Red)';
-    
+
     await page.locator('#item_3_title_link').click();
     await expect(page.locator('.inventory_details_name')).toHaveText(product);
     
     await page.getByRole('button', { name: 'Add to cart' }).click();
     await expect(page.getByRole('button', { name: 'Remove' })).toHaveText(/Remove/);
     await expect(page.locator('.shopping_cart_badge')).toHaveText(/1/);
-  })
+  });
 
   test('add product to cart by inventory page', async ({ page }) => {
     // Product = Sauce Labs Bike Light
     await page.locator('#add-to-cart-sauce-labs-bike-light').click();
     await expect(page.locator('#remove-sauce-labs-bike-light')).toHaveText(/Remove/);
     await expect(page.locator('.shopping_cart_badge')).toHaveText(/1/);
-  })
+  });
+
+  test('checkout', async ({ page }) => {
+    let product = 'Sauce Labs Bike Light';
+    let firstname = 'John';
+    let lastname = 'Smith';
+    let zipcode = '12345';
+
+    // Test setup: Add product (Sauce Labs Bike Light) to cart
+    await page.locator('#add-to-cart-sauce-labs-bike-light').click();
+    await expect(page.locator('#remove-sauce-labs-bike-light')).toHaveText(/Remove/);
+    await expect(page.locator('.shopping_cart_badge')).toHaveText(/1/);
+    await page.locator('.shopping_cart_link').click();
+
+    // Could just assert page by URL if there are other test verifying the contents of the page
+    // await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
+    await expect(page.locator('.title')).toHaveText(/Your Cart/);
+    await expect(page.locator('.inventory_item_name')).toHaveText(product);
+
+    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    // Same here - could assert page by URL if other test(s) verify contents of page
+    // await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-one.html');
+    await expect(page.locator('.title')).toHaveText(/Checkout: Your Information/);
+    await page.locator('#first-name').fill(firstname);
+    await page.locator('#last-name').fill(lastname);
+    await page.locator('#postal-code').fill(zipcode);
+
+    await page.locator('#continue').click();
+
+    // Same here - could assert page by URL if other test(s) verify contents of page
+    // await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-two.html');
+    await expect(page.locator('.title')).toHaveText(/Checkout: Overview/);
+    await expect(page.locator('.inventory_item_name')).toHaveText(product); 
+    
+    await page.locator('#finish').click();
+
+    // Same here - could assert page by URL if other test(s) verify contents of page
+    await expect(page).toHaveURL('https://www.saucedemo.com/checkout-complete.html');
+    await expect(page.locator('.title')).toHaveText(/Checkout: Complete!/);
+    await expect(page.locator('h2.complete-header')).toHaveText('Thank you for your order!');
+
+    await page.getByRole('button', {name: 'Back Home' }).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  });
 });
